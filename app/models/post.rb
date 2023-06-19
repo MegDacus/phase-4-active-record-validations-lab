e90c9c400e3f9cafc4bc4b81dbe296a1a9cef0f3
MegDacus <124758848+MegDacus@users.noreply.github.com>
2023-06-19 15:11:27 +1000

    Assignment Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+    def clickbait?
+    
+    clickbait_array = ["Won't Believe", "Secret", "Top \\d+", "Guess"]
+    
+        if title.present? && clickbait_array.none?{|phrase| title.match?(phrase)}
+            errors.add(:title, "must be clickbait")
+            false
+        else 
+            true
+        end
+    end
+
 end
